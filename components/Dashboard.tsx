972a5f9752caccb4b4180c8ff987d898f54dd6d7
Victor <victoroprea49@gmail.com>
2023-07-13 14:07:52 +0300

    Added Links for Dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Dashboard, Inventory, LocalShipping } from '@mui/icons-material'
-import { Avatar, ListItemButton, ListItemIcon, ListItemText, Collapse, List } from '@mui/material'
+import Link from 'next/link'
+import { Dashboard, Inventory, LocalShipping, KeyboardArrowRight, Login } from '@mui/icons-material'
+import { Avatar, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { blue } from '@mui/material/colors'
 import styles from '@/styles/Dashboard.module.css'
 
@@ -10,28 +11,46 @@ export default function DashboardTab() {
     return (
         <div className={styles.main}>
             <div className={styles.content}>
-                <Avatar sx={{ bgcolor: blue[500], width: 56, height: 56 }} />
-                <h1 className={styles.name}>JOHN</h1>
-                <h1 className={styles.pos}>CYBER CLOTHING</h1>
+                <div className={styles.userProfile}>
+                    <Avatar sx={{ bgcolor: blue[500], width: 56, height: 56 }} />
+                    <h1 className={styles.name}>JOHN</h1>
+                    <h1 className={styles.pos}>CYBER CLOTHING</h1>
+                </div>
                 <div>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <Dashboard />
-                        </ListItemIcon>
-                        <ListItemText primary="Dashboard" />
-                    </ListItemButton>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <Inventory />
-                        </ListItemIcon>
-                        <ListItemText primary="Products" />
-                    </ListItemButton>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <LocalShipping />
-                        </ListItemIcon>
-                        <ListItemText primary="Orders" />
-                    </ListItemButton>
+                    <Link href="/backend">
+                        <ListItemButton className={styles.btn} >
+                            <ListItemIcon>
+                                <Dashboard />
+                            </ListItemIcon>
+                            <ListItemText primary="Dashboard" />
+                        </ListItemButton>
+                    </Link>
+                    <Link href="/backend/products">
+                        <ListItemButton className={styles.btn}>
+                            <ListItemIcon>
+                                <Inventory />
+                            </ListItemIcon>
+                            <ListItemText primary="Products" />
+                            <KeyboardArrowRight fontSize="small" />
+                        </ListItemButton>
+                    </Link>
+                    <Link href="/backend/orders">
+                        <ListItemButton className={styles.btn}>
+                            <ListItemIcon>
+                                <LocalShipping />
+                            </ListItemIcon>
+                            <ListItemText primary="Orders" />
+                            <KeyboardArrowRight fontSize="small" />
+                        </ListItemButton>
+                    </Link>
+                    <Link href="/backend/login">
+                        <ListItemButton className={styles.btn}>
+                            <ListItemIcon>
+                                <Login />
+                            </ListItemIcon>
+                            <ListItemText primary="Login" />
+                        </ListItemButton>
+                    </Link>
                 </div>
             </div>
         </div >
