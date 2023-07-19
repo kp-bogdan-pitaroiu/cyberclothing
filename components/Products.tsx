f93826b4e6ca63a9bf3940fc071dac1ef218625b
Victor <victoroprea49@gmail.com>
2023-07-19 15:56:16 +0300

    Added Products on Homepage and Product Page

---
+++ components/Products.tsx
@@ -0,0 +1,166 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '@/styles/Products.module.css'
+
+const Products = () => {
+    return (
+        <>
+            <div className={styles.products}>
+                <h4>Exclusive Products</h4>
+                <h2>SPECIAL PRODUCTS</h2>
+            </div>
+            <div className={styles.ul}>
+                <Link href="/frontend/products/1">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/1.jpg"></img></li>
+                        <li> <h6>Flare Dress</h6> </li>
+                        <li> <h4>$ 60</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/2.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/3.jpg"></img></li>
+                        <li> <h6>Floral Dress</h6> </li>
+                        <li> <h4>$ 278</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/4.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+            </div>
+            <div className={styles.ul}>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/5.jpg"></img></li>
+                        <li> <h6>Flare Dress</h6> </li>
+                        <li> <h4>$ 60</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/6.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/7.jpg"></img></li>
+                        <li> <h6>Floral Dress</h6> </li>
+                        <li> <h4>$ 278</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/8.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+            </div>
+            <div className={styles.ul}>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/43.jpg"></img></li>
+                        <li> <h6>Cut Tshirt</h6> </li>
+                        <li> <h4>$ 125</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/10.jpg"></img></li>
+                        <li> <h6>Shortsleeve Tshirt</h6> </li>
+                        <li> <h4>$ 125</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/11.jpg"></img></li>
+                        <li> <h6>Maxi Dress</h6> </li>
+                        <li> <h4>$ 70</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/12.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+            </div>
+            <div className={styles.ul}>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/13.jpg"></img></li>
+                        <li> <h6>Flare Dress</h6> </li>
+                        <li> <h4>$ 60</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/32.jpg"></img></li>
+                        <li> <h6>Mens Jeans</h6> </li>
+                        <li> <h4>$ 300</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/45.jpg"></img></li>
+                        <li> <h6>Maxi Dress</h6> </li>
+                        <li> <h4>$ 70</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/16.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+            </div>
+            <div className={styles.ul}>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/17.jpg"></img></li>
+                        <li> <h6>Trim Dress</h6> </li>
+                        <li> <h4>$ 210</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/18.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 165</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/19.jpg"></img></li>
+                        <li> <h6>Floral Dress</h6> </li>
+                        <li> <h4>$ 278</h4> </li>
+                    </ul>
+                </Link>
+                <Link href="/frontend/products">
+                    <ul className={styles.li}>
+                        <li> <img src="https://react.pixelstrap.com/assets/images/fashion/product/20.jpg"></img></li>
+                        <li> <h6>Sleeveless Dress</h6> </li>
+                        <li> <h4>$ 475</h4> </li>
+                    </ul>
+                </Link>
+            </div>
+        </>
+    )
+}
+
+export default Products
--- pages/frontend/index.tsx
+++ pages/frontend/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '@/components/Header'
+import Products from '@/components/Products'
 
 const index = () => {
     return (
         <>
             <Header />
+            <Products />
         </>
     )
 }
